de5410e2a720570787064c86a7c7093381ff48a7
kirstinramos <kirstinramos@merchantsfleet.com>
2022-11-14 17:50:56 +0800

    Session2: 1st commit: provided all answers to TO DOs

--- Session1/EFCoreAssignment/Program.cs
+++ Session1/EFCoreAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCoreAssignment;
+using EFCoreAssignment.Entities;
 using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
@@ -19,6 +20,11 @@ SingleOrDefault(dbContext);
 LoadingRelatedData_Manual(dbContext);
 LoadingRelatedData_ExplicitLoading(dbContext);
 LoadingRelatedData_EagerLoading(dbContext);
+InsertProduct(dbContext);
+InsertProductWithNewShop(dbContext);
+UpdateProduct(dbContext);
+DeleteProduct(dbContext);
+DeleteProductByKey(dbContext);
 
 static void Filtering(AppDbContext dbContext)
 {
@@ -64,5 +70,75 @@ static void LoadingRelatedData_EagerLoading(AppDbContext dbContext)
         .FirstOrDefault();
 }
 
+static void InsertProduct(AppDbContext dbContext)
+{
+    // TODO: Insert a new Product
+    var product = new Product
+    {
+        Name = "Apple Watch Ultra",
+        ShopId = 1
+    };
+    dbContext.Products.Add(product);
+    dbContext.SaveChanges();
+
+    var products = dbContext.Products
+        .Where(a => a.Name == "Apple Watch Ultra")
+        .ToList();
+}
+
+static void InsertProductWithNewShop(AppDbContext dbContext)
+{
+    // TODO: Insert a new Product with a new Shop
+    var product = new Product
+    {
+        Name = "Samsung Galaxy Fold 4",
+        Shop = new Shop
+        {
+            Name = "Mega Tech"
+        }
+    };
+    dbContext.Products.Add(product);
+    dbContext.SaveChanges();
+
+    var products = dbContext.Products
+        .Where(a => a.Name == "Samsung Galaxy Fold 4")
+        .ToList();
+}
+
+static void UpdateProduct(AppDbContext dbContext)
+{
+    var products = dbContext.Products
+        .Where(a => a.Id == 2)
+        .ToList();
+
+    // TODO: Update a Product
+    var productId = 1;
+    var product = dbContext.Products.Single(b => b.Id == 2);
+
+    product.Name = "Apple Mouse Lightweight";
+    dbContext.SaveChanges();
+
+    var updateProducts = dbContext.Products
+        .Where(a => a.Id == 2)
+        .ToList();
+}
+
+static void DeleteProduct(AppDbContext dbContext)
+{
+    // TODO: Delete a Product
+    var productId = 14;
+    var product = dbContext.Products.Single(b => b.Id == productId);
+    dbContext.Products.Remove(product);
+    dbContext.SaveChanges();
+}
+
+static void DeleteProductByKey(AppDbContext dbContext)
+{
+    // TODO: Delete a Product with just having a key
+    var productId = 13;
+    var product = new Product { Id = productId };
+    dbContext.Products.Remove(product);
+    dbContext.SaveChanges();
+}
 
 Console.WriteLine("EF Core is the best");
