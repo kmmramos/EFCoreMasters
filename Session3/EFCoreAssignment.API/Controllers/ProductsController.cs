da84231cbab8db5b6626887e6297050e41442882
kirstinramos <kirstinramos@merchantsfleet.com>
2022-11-17 16:51:03 +0800

    Session3: 1st-commit: Completed all of To Dos

---
+++ Session3/EFCoreAssignment.API/Controllers/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using EFCoreAssignment.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreAssignment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var result = await _service.GetProducts();
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var result = await _service.GetProduct(id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductApiModel vm)
+        {
+            try
+            {
+                var result = await _service.CreateProduct(new CreateProductDto(vm.Name, vm.ShopId));
+
+                if (result == 0)
+                    return BadRequest();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductApiModel vm)
+        {
+            try
+            {
+                await _service.UpdateProduct(new UpdateProductDto(vm.Id, vm.Name, vm.ShopId));
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }            
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            try
+            {
+                await _service.DeleteProduct(id);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+
+    public record CreateProductApiModel(string Name, int ShopId);
+    public record UpdateProductApiModel(int Id, string Name, int ShopId);
+}
---
+++ Session3/EFCoreAssignment.Services/ProductService.cs
@@ -0,0 +1,127 @@
+﻿using EFCoreAssignment.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreAssignment.Data.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext _context;
+
+        public ProductService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProductDto>> GetProducts()
+        {
+            // TODO get products
+            var productDto = await _context.Products.Select(i => new ProductDto(i.Id, i.Name, i.ShopId)).ToListAsync();
+
+            if (productDto == null)
+                return null;
+
+            return productDto;
+        }
+
+        public async Task<ProductDto> GetProduct(int id)
+        {
+            // TODO get product
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return null;
+
+            var productDto = new ProductDto(product.Id, product.Name, product.ShopId);
+            return productDto;
+        }
+
+        public async Task<int> CreateProduct(CreateProductDto productForCreation)
+        {
+            // TODO create a product
+            var product = new Product()
+            {
+                Name = productForCreation.Name,
+                ShopId = productForCreation.ShopId
+            };
+
+            _context.Products.Add(product);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+
+            int createdProductId = GetProduct(product.Id).Result.Id;
+            if (createdProductId == 0)
+                return 0;
+
+            return createdProductId;
+        }
+
+        public async Task UpdateProduct(UpdateProductDto productForUpdate)
+        {
+            //TODO update a product
+            var product = new Product()
+            {
+                Id = productForUpdate.Id,
+                Name = productForUpdate.Name,
+                ShopId = productForUpdate.ShopId
+            };
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                if (!ProductExists(productForUpdate.Id))
+                    throw new DbUpdateConcurrencyException();
+                else
+                    throw;
+            }
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            //TODO delete a product
+            var productToBeDeleted = await _context.Products.FindAsync(id);
+            if (productToBeDeleted == null)
+                throw new ArgumentNullException("Product is not found!");
+
+            _context.Products.Remove(productToBeDeleted);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+
+    }
+
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetProducts();
+        Task<ProductDto> GetProduct(int id);
+        Task<int> CreateProduct(CreateProductDto productForCreation);
+        Task UpdateProduct(UpdateProductDto productForUpdate);
+        Task DeleteProduct(int id);
+    }
+
+    public record ProductDto(int Id, string Name, int ShopId);
+    public record CreateProductDto(string Name, int ShopId);
+    public record UpdateProductDto(int Id, string Name, int ShopId);
+}
